67fcda3148d21a1617905ff1bbcbfe4f7b34e804
CecileLecoublet <lecoublet.cecile@gmail.com>
2023-01-11 17:09:46 +0100

    Taille du point

--- Dashboard/Feat_globale.py
+++ Dashboard/Feat_globale.py
@@ -30,7 +30,8 @@ def fc_global(X_test_scaled, X_train_scaled, choix) :
     position_colonne = X_test_scaled.columns.get_loc("AMT_GOODS_PRICE")
     fig_1 = px.scatter(shap_values[0][position_colonne])
     prov = X_test_scaled[X_test_scaled["SK_ID_CURR"] == choix]
-    fig_2 = px.scatter(prov['AMT_GOODS_PRICE'], color_discrete_sequence=['white'], marker_size = [115, 20, 30])
+    fig_2 = px.scatter(prov['AMT_GOODS_PRICE'], color_discrete_sequence=['white'])
+    fig_2.update_traces(marker={'size': 30})
     plt.title("Rente depence plot",loc='left',fontfamily='serif',fontsize=15)
     plt.ylabel("SHAP value for the 'AMT_GOODS_PRICE' feature")
     st.write(fig_1.add_trace(fig_2.data[0]))
