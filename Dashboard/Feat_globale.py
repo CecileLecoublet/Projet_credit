0781049624d20676f47274f31bf33f84519baa4b
CecileLecoublet <lecoublet.cecile@gmail.com>
2023-01-11 15:00:56 +0100

    Test

--- Dashboard/Feat_globale.py
+++ Dashboard/Feat_globale.py
@@ -27,7 +27,7 @@ def fc_global(X_test_scaled, X_train_scaled, choix) :
     st.set_option('deprecation.showPyplotGlobalUse', False)
     fig = shap.summary_plot(shap_values[0], X_test_scaled, plot_type="bar")
     st.pyplot(fig)
-    fig_1 = shap.dependence_plot('AMT_GOODS_PRICE', shap_values[0], X_test_scaled, interaction_index="AMT_GOODS_PRICE", add_stat_cor = True)
+    fig_1 = shap.dependence_plot('AMT_GOODS_PRICE', shap_values, X_test_scaled, interaction_index="AMT_GOODS_PRICE")
     # plt.title("Rente depence plot",loc='left',fontfamily='serif',fontsize=15)
     # plt.ylabel("SHAP value for the 'AMT_GOODS_PRICE' feature")
     st.pyplot(fig_1)
