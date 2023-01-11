6d4edfa4a8b6da5cc41edc605187e9aa4c7823ab
CecileLecoublet <lecoublet.cecile@gmail.com>
2023-01-11 11:38:59 +0100

    Test

--- Dashboard/Feat_globale.py
+++ Dashboard/Feat_globale.py
@@ -20,10 +20,11 @@ def load_model():
 def fc_global(X_test_scaled, X_train_scaled, choix) :
     st.markdown("## Cinquième chapitre : Features global et features local")
     # Entraînement
-    explainer = shap.Explainer(load_model().predict, X_test_scaled)
+    explainer = shap.TreeExplainer(load_model())
     # Calculates the SHAP values - It takes some time
-    shap_values = explainer(X_test_scaled)
+    shap_values = explainer.shap_values(X_test_scaled)
     # Evaluate SHAP values
-    fig = shap.plots.bar(shap_values)
-    st.write(fig)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
+    fig = shap.summary_plot(shap_values[0], X_test_scaled, plot_type="bar")
+    st.pyplot(fig)
     
