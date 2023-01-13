4fb1b8dca35637a0fc5b8c16dd04119b0d31a5d6
CecileLecoublet <lecoublet.cecile@gmail.com>
2023-01-13 12:14:20 +0100

    Retour en arrière

--- Dashboard/Feat_globale.py
+++ Dashboard/Feat_globale.py
@@ -24,8 +24,7 @@ def choix_var(X_test_scaled, shap_values, nom):
     nom = nom[feat['col_name'].to_list()]
     nom = nom.select_dtypes(exclude = 'object')
     choix = st.selectbox("Choix des features les plus importantes", nom.columns)
-    st.write("Une autre sélection de features")
-    choix_2 = st.selectbox(feat['col_name'].to_list())
+    choix_2 = st.selectbox("Choix des features les plus importantes", nom.columns[nom.columns != choix])
     position = X_test_scaled.columns.get_loc(choix)
     return position, choix, choix_2
 
