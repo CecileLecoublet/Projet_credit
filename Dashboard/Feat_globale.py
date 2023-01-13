78785fa344f6ea9d9e71d9ac94cee17dcb1932f5
CecileLecoublet <lecoublet.cecile@gmail.com>
2023-01-13 10:45:31 +0100

    Correction des appelles du graphes shap

--- Dashboard/Feat_globale.py
+++ Dashboard/Feat_globale.py
@@ -28,7 +28,7 @@ def choix_var(X_test_scaled, shap_values, nom):
     var = feat[feat["col_name"] == choix]
     #var_2 = feat[feat["col_name"] == choix_2]
     position = X_test_scaled.columns.get_loc(choix)
-    return position
+    return position, choix
 
 # Shap garphique
 def dep_plt(i, col, color_by, base_actual_df, base_shap_df, overlay_x, overlay_y):
@@ -68,11 +68,11 @@ def fc_global(X_test_scaled, X_test, X_train_scaled, nom) :
     j = 0
     X_train = X_train_scaled[0:122]
     shap_values_train = explainer.shap_values(X_train_scaled)
-    position_1 = choix_var(X_test_scaled, shap_values, nom)
+    position_1, choix = choix_var(X_test_scaled, shap_values, nom)
     for i in range(0, len(imp_cols)):
         #plot the top var and color by the 2nd var
         if i == 0 : 
-            fig = dep_plt(i, imp_cols[position_1], imp_cols[position_1], 
+            fig = dep_plt(i, choix, choix, 
             X_train, 
             shap_values_train[0][i],
             X_test_scaled.iloc[j,:][imp_cols[i]], 
