b572a88d717d93f06998a313f0c9304345123f02
CecileLecoublet <lecoublet.cecile@gmail.com>
2023-01-16 10:04:00 +0100

    test

--- Dashboard/Feat_globale.py
+++ Dashboard/Feat_globale.py
@@ -73,8 +73,8 @@ def fc_global(X_test_scaled, X_test, X_train_scaled, nom, numero) :
     position_1, choix, choix_2 = choix_var(X_test_scaled, shap_values, nom)
     #plot the top var and color by the 2nd var
     st.write(numero)
-    i = X_test_scaled[X_test_scaled["SK_ID_CURR"] == choix].index
-    st.write(X_test_scaled[X_test_scaled["SK_ID_CURR"] == choix])
+    i = X_test_scaled[X_test_scaled["SK_ID_CURR"] == numero].index
+    st.write(X_test_scaled[X_test_scaled["SK_ID_CURR"] == numero])
     st.write(i)
     fig = dep_plt(i, choix, choix_2, 
     X_train, 
